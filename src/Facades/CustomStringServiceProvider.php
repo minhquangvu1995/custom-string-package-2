3202a8eaf5546bccdd1063e5dd4f67f4f71bebef
Minh <minhvu2904@gmail.com>
2022-05-27 09:17:26 +0700

    test

--- src/Facades/CustomStringServiceProvider.php
+++ src/Facades/CustomStringServiceProvider.php
@@ -6,8 +6,6 @@ use Illuminate\Support\ServiceProvider;
 
 class CustomStringServiceProvider extends ServiceProvider
 {
-    protected $defer = false;
-
     public function register()
     {
         $this->app->bind('customString', function($app) {
